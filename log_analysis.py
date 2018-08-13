3a9603cd123e2891722fce449395dbd07d65a837
王润家 <wangrunjia@wangrunjiadeMBP.domain>
2018-08-13 10:18:14 -0700

    modify the CODE to standard. Correct some bugs and correct incomplete anwser.

--- log_analysis.py
+++ log_analysis.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import psycopg2
 from datetime import datetime
 
 DBNAME = 'news'
 
-def queryOperate(query):
 
+def queryOperate(query):
 	try:
-		db = psycopg2.connect(database = DBNAME)
+		db = psycopg2.connect(database=DBNAME)
+		"""
+		Exception raised for errors 
+		that are related to the database’s operation 
+		and not necessarily under the control of the programmer
+		"""
 	except psycopg2.OperationalError as e:
-		print "there is an error when connect to database"+ DBNAME
+		print "there is an error when connect to database" + DBNAME
 		print e
 
 	c = db.cursor()
@@ -32,12 +39,12 @@ def top_articles():
 
 
 def topAuthors():
-	query = """ SELECT authors.name, COUNT(*) AS views 
-				FROM authors 
-				JOIN articles ON articles.author = authors.id 
-				JOIN log ON log.path = '/article/' || articles.slug 
-				WHERE log.status LIKE '200%' 
-				GROUP BY authors.name 
+	query = """ SELECT authors.name, COUNT(*) AS views
+				FROM authors
+				JOIN articles ON articles.author = authors.id
+				JOIN log ON log.path = '/article/' || articles.slug
+				WHERE log.status LIKE '200%'
+				GROUP BY authors.name
 				ORDER BY views DESC;"""
 	authorsRank = queryOperate(query)
 	return authorsRank
@@ -46,12 +53,12 @@ def topAuthors():
 def errorLimit():
 	query = """ SELECT day, percent FROM(
 				SELECT day, ROUND(
-				(SUM(error)/(SELECT COUNT(*) FROM log 
+				(SUM(error)/(SELECT COUNT(*) FROM log
 				WHERE time::date = day)*100),2) AS percent FROM (
-				SELECT time::date AS day, 
-				COUNT(*) AS error 
-				FROM log 
-				WHERE status LIKE '%404%' 
+				SELECT time::date AS day,
+				COUNT(*) AS error
+				FROM log
+				WHERE status LIKE '%404%'
 				GROUP BY day)
 				AS percentlist
 				GROUP BY day
@@ -67,18 +74,18 @@ if __name__ == '__main__':
 	authorsRank = topAuthors()
 	errorcount = errorLimit()
 
-	with open('output.txt','w') as f:
+	with open('output.txt', 'w') as f:
 		q1 = '1. what are the most popular three articles of all time?\n'
 		f.write(q1)
 		for item in topArticles:
 			f.write(item[0] + ' --'+str(item[1]) + ' views \n')
 		f.write('\n2. What are the most popular article authors of all time \n')
 		for name in authorsRank:
-			f.write(name[0]+' --' +str(name[1])+' views \n')
+			f.write(name[0] + ' --' +str(name[1]) + ' views \n')
 		f.write('\n3. On which days did more than 1% of requests lead to errors?\n')
 		for error in errorcount:
 			changetime = datetime.strptime(str(error[0]), '%Y-%m-%d').strftime('%B %d, %Y')
-			f.write(changetime +' --'+ str(error[1])+'% errors')
+			f.write(changetime + ' --' + str(error[1]) + '% errors')
 
 
 
